f6bd3356157c007cb5401bba72e850aaea4cf689
taylormerrill <taylormerrill92@gmail.com>
2018-11-13 15:02:46 -0700

    added error handling

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,6 +1,6 @@
 const logger= function(req, res, next){
 const time = new Date(); 
-console.log(` ${time.toDateString()}, ${time.toLocaleTimeString('en-US')}, ${req.method} ${req.url}`); 
+console.log(`${time.toDateString()}, ${time.toLocaleTimeString('en-US')}, ${req.method} ${req.url}`); 
 next();     
 }
 module.exports= {logger};
--- server.js
+++ server.js
@@ -5,12 +5,11 @@ const express = require('express');
 const data = require('./db/notes');
 const app = express();
 const { PORT } = require('./config');
-const { logger } = require('./middleware/logger'); 
-app.listen(PORT, function () {
-  console.info(`Server listening on  ${PORT}`);
-}).on('error', err => {
-  console.error(err);
-});
+const { logger } = require('./middleware/logger');
+
+
+
+
 app.use(logger); 
 app.get('/api/notes', (req, res) => {
    const search = req.query.search; 
@@ -25,7 +24,7 @@ app.get('/api/notes', (req, res) => {
   res.json(data);
   }
   });
-
+ 
   app.get('/api/notes/:id',(req, res)=> {
     const id = req.params.id; 
     let note = data.find(item=>item.id === Number(id));  
@@ -33,4 +32,24 @@ app.get('/api/notes', (req, res) => {
   });
 console.log('Hello Noteful!');
 
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  res.status(404).json({ message: 'Not Found' });
+  next(); 
+}); 
+
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: err
+  });
+});
+
+app.listen(PORT, function () {
+  console.info(`Server listening on  ${PORT}`);
+}).on('error', err => {
+  console.error(err);
+});
 
